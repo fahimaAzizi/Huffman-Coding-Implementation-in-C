ee7c794837924bf34c882893a482d59f3613cd15
Zakir Aziz <your-email@example.com>
2024-10-08 20:12:09 +0500

    committed

---
+++ code of c/Huffman Coding Implementation in.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TREE_HT 100
+
+// A Huffman tree node
+struct MinHeapNode {
+    char data; // One of the input characters
+    unsigned freq; // Frequency of the character
+    struct MinHeapNode *left, *right; // Left and right children
+};
+
+// A MinHeap: Collection of min heap (or Hufmman tree) nodes
+struct MinHeap {
+    unsigned size; // Current size of min heap
+    unsigned capacity; // capacity of min heap
+    struct MinHeapNode** array; // Array of minheap node pointers
+};
+
+// Utility function to allocate a new min heap node with given character and frequency of the character
+struct MinHeapNode* newNode(char data, unsigned freq) {
+    struct MinHeapNode* temp = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
+
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+
+    return temp;
+}
+
+// Utility function to create a min heap of given capacity
+struct MinHeap* createMinHeap(unsigned capacity) {
+    struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
+
+    minHeap->size = 0; // current size is 0
+    minHeap->capacity = capacity;
+    minHeap->array = (struct MinHeapNode**)malloc(minHeap->capacity * sizeof(struct MinHeapNode*));
+
+    return minHeap;
+}
+
+// Utility function to swap two min heap nodes
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b) {
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
+// The standard minHeapify function
+void minHeapify(struct MinHeap* minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx) {
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
